20a49f9c7675eed558e7f11f3736a3113eea1330
Lami <Halam.Lee.Lami@gmail.com>
2023-09-11 16:05:16 +0900

    feat: ColorPicker 컴포넌트 생성

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,42 @@
+<template>
+  <ColorPicker
+    is-widget
+    format="hex"
+    shape="square"
+    picker-type="chrome"
+    use-type="pure"
+    disable-history
+    disable-alpha
+    :pure-color="currentColor"
+    v-on:update:pureColor="emitChangeColor" />
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+const props = defineProps({
+  currentColor: String,
+});
+
+const emit = defineEmits(['change-color']);
+
+const emitChangeColor = (color) => {
+  emit('change-color', color);
+};
+
+const currentColor = ref(props.currentColor);
+
+watch(
+  () => props.currentColor,
+  (newColor) => {
+    currentColor.value = newColor;
+    console.log('pureColor', props.currentColor);
+  }
+);
+</script>
+
+<style>
+.vc-colorpicker {
+  width: 300px !important;
+}
+</style>
