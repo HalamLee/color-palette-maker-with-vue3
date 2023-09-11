14a7abad5280b88b253acc2dfa1ce6e1cd76e830
Lami <Halam.Lee.Lami@gmail.com>
2023-09-11 16:11:11 +0900

    feat: 기존 HomeView 삭제 후 라우터 설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import PaletteView from '../views/PaletteView.vue';
+import PaletteEditView from '../views/PaletteEditView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +8,13 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: PaletteView,
+    },
+
+    {
+      path: '/edit',
+      name: 'paletteEditView',
+      component: PaletteEditView,
     },
   ],
 });
