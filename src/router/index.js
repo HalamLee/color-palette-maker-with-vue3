dfa5643d12006be52eff61fcfb31a168466d4bcc
Lami <Halam.Lee.Lami@gmail.com>
2023-09-12 10:52:42 +0900

    feat: 404 페이지 생성 및 라우터 연결

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import PaletteView from '../views/PaletteView.vue';
 import PaletteCreateView from '../views/PaletteCreateView.vue';
 import PaletteEditView from '../views/PaletteEditView.vue';
+import NotFoundView from '../views/NotFoundView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,15 @@ const router = createRouter({
       path: '/edit',
       name: 'paletteEditView',
       component: PaletteEditView,
+    },
+    {
+      path: '/404',
+      name: 'notFound',
+      component: NotFoundView,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/404',
     },
   ],
 });
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>페이지를 찾을 수 없습니다.</p>
+    <p>URL을 확인해주세요.</p>
+    <RouterLink to="/"><b>Home으로 가기</b></RouterLink>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  text-align: center;
+}
+
+a {
+  display: block;
+  margin-top: 50px;
+  text-decoration: none;
+  color: inherit;
+  padding: 10px;
+  background-color: rgb(39, 100, 178);
+  border-radius: 5px;
+  color: white;
+}
+</style>
